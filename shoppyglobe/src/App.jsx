0cd4654f26d7d6c582a90c776b26b422c57707d6
sana2990 <sanaasad2990@gmail.com>
2026-05-22 17:11:05 +0530

    updating cart item number functionality added

--- shoppyglobe/src/App.jsx
+++ shoppyglobe/src/App.jsx
@@ -14,10 +14,59 @@ function App() {
 
   const [cart, setCart] = useState([]);
 
-    const handleAddToCart = (product) => {
-    setCart([...cart, product]);
-  };
+   const handleAddToCart = (product) => {
 
+  const existingItem = cart.find(
+    (item) => item.id === product.id
+  );
+
+  if (existingItem) {
+
+    const updatedCart = cart.map((item) =>
+      item.id === product.id
+        ? {
+            ...item,
+            quantity: item.quantity + 1
+          }
+        : item
+    );
+
+    setCart(updatedCart);
+
+  } else {
+
+    setCart([
+      ...cart,
+      {
+        ...product,
+        quantity: 1
+      }
+    ]);
+
+  }
+};
+
+  const increaseQuantity = (id) => {
+
+  const updatedCart = cart.map((item) =>
+    item.id === id
+      ? { ...item, quantity: item.quantity + 1 }
+      : item
+  );
+
+  setCart(updatedCart);
+};
+
+const decreaseQuantity = (id) => {
+
+  const updatedCart = cart.map((item) =>
+    item.id === id && item.quantity > 1
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  );
+
+  setCart(updatedCart);
+};
 
   return (
     <>
@@ -28,7 +77,7 @@ function App() {
           <Route path="/product/:id" element={<ProductItem handleAddToCart={handleAddToCart} />}  />
 
           <Route path="/cart"
-          element= {<Cart cart={cart} />} />
+          element= {<Cart cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />} />
         </Routes>
       
     </>
--- shoppyglobe/src/components/Cart.jsx
+++ shoppyglobe/src/components/Cart.jsx
@@ -1,19 +1,56 @@
-function Cart({cart}) {
+function Cart({
+  cart,
+  increaseQuantity,
+  decreaseQuantity
+}) {
+
     return (
         <div>
+
             <h1>Cart</h1>
 
-            {cart.length === 0? (<h3>Cart is empty</h3>): (
+            {cart.length === 0 ? (
+
+                <h3>Cart is empty</h3>
+
+            ) : (
+
                 cart.map((item) => (
-                    <div key ={item.id}>
+
+                    <div key={item.id}>
+
                         <h2>{item.title}</h2>
 
-                        <img src={item.thumbnail} alt={item.title} width="150"></img>
+                        <img
+                          src={item.thumbnail}
+                          alt={item.title}
+                          width="150"
+                        />
 
                         <p>${item.price}</p>
+
+                        <button
+                          onClick={() => decreaseQuantity(item.id)}
+                        >
+                          -
+                        </button>
+
+                        <input
+                          type="number"
+                          value={item.quantity}
+                          readOnly
+                        />
+
+                        <button
+                          onClick={() => increaseQuantity(item.id)}
+                        >
+                          +
+                        </button>
+
                     </div>
                 ))
             )}
+
         </div>
     );
 }
