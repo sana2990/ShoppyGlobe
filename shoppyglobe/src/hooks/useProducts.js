2328509bad4deadec1e393e57e9a7ea14e393835
sana2990 <sanaasad2990@gmail.com>
2026-05-22 07:17:12 +0530

    added hooks folder

---
+++ shoppyglobe/src/hooks/useProducts.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await response.json();
+
+        setProducts(data.products);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return { products, loading, error };
+}
+
+export default useProducts;
